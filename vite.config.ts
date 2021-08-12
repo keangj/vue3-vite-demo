6407aa075ec4f0cae7bd7e136c38d845369424ba
jay <keangjay@gmail.com>
2021-08-12 15:04:22 +0800

    配置别名

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,14 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import { resolve } from 'path'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()]
+  plugins: [vue()],
+  resolve: {
+    alias: {
+      "src": resolve(__dirname, "src"),	// 配置根目录别名
+      "comps": resolve(__dirname, "src/components"),	// 配置组件目录别名
+    }
+  },
 })
