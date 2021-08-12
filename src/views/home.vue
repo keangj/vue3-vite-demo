25663809126fc35e48ba1e7a2a5e490bbfc0167e
jay <keangjay@gmail.com>
2021-08-12 15:20:59 +0800

    添加 vue-router

---
+++ src/views/home.vue
@@ -0,0 +1,14 @@
+<template>
+  <button type="button" @click="count++">count is: {{ count }}</button>
+</template>
+
+<script lang="ts">
+import { ref, defineComponent } from 'vue'
+export default defineComponent({
+  name: 'HelloWorld',
+  setup: () => {
+    const count = ref(0)
+    return { count }
+  }
+})
+</script>
