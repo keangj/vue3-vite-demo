eb66e927e651a1c94c78674e7480b4995d127968
jay <keangjay@gmail.com>
2021-08-13 10:32:03 +0800

    格式化代码

--- .eslintrc.js
+++ .eslintrc.js
@@ -4,19 +4,12 @@ module.exports = {
     es2021: true,
     node: true
   },
-  extends: [
-    'plugin:vue/vue3-essential',
-    'standard'
-  ],
+  extends: ['plugin:vue/vue3-essential', 'standard'],
   parserOptions: {
     ecmaVersion: 12,
     parser: '@typescript-eslint/parser',
     sourceType: 'module'
   },
-  plugins: [
-    'vue',
-    '@typescript-eslint'
-  ],
-  rules: {
-  }
+  plugins: ['vue', '@typescript-eslint'],
+  rules: {}
 }
--- src/App.vue
+++ src/App.vue
@@ -12,5 +12,4 @@ export default defineComponent({
 })
 </script>
 
-<style>
-</style>
+<style></style>
