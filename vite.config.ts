8f06b270a5e293b1edc091df5d9891c70456b388
jay <keangjay@gmail.com>
2022-04-13 11:24:44 +0800

    chore(build): 打包优化

--- vite.config.ts
+++ vite.config.ts
@@ -1,46 +1,66 @@
-import { defineConfig } from 'vite'
+import type { UserConfig, ConfigEnv } from 'vite'
+import { loadEnv } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import { resolve } from 'path'
 import vueJsx from '@vitejs/plugin-vue-jsx'
+import { configCompressPlugin } from './build/plugin/compress'
+import { configImageminPlugin } from './build/plugin/imagemin'
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [vue(), vueJsx()],
-  resolve: {
-    alias: {
-      src: resolve(__dirname, 'src'),
-      comps: resolve(__dirname, 'src/components'),
-      api: resolve(__dirname, 'src/api'),
-      hooks: resolve(__dirname, 'src/hooks'),
-      types: resolve(__dirname, 'types')
-    }
-  },
-  css: {
-    preprocessorOptions: {
-      scss: {
-        additionalData: '@import "src/styles/var/_var.scss";'
-      }
-    }
-  },
-  build: {
-    minify: 'terser',
-    terserOptions: {
-      compress: {
-        // 生产环境时移除console
-        drop_console: true,
-        drop_debugger: true
+export default ({ mode }: ConfigEnv): UserConfig => {
+  const root = process.cwd()
+  const env = loadEnv(mode, root)
+  const {
+    VITE_BUILD_COMPRESS,
+    VITE_BUILD_COMPRESS_DELETE_ORIGIN_FILE,
+    VITE_DROP_CONSOLE,
+    VITE_USE_IMAGEMIN
+  } = env
+  return {
+    plugins: [
+      vue(),
+      vueJsx(),
+      configCompressPlugin(
+        VITE_BUILD_COMPRESS as 'gzip' | 'brotli' | 'none',
+        VITE_BUILD_COMPRESS_DELETE_ORIGIN_FILE === 'true'
+      ),
+      VITE_USE_IMAGEMIN === 'true' && configImageminPlugin()
+    ],
+    resolve: {
+      alias: {
+        src: resolve(__dirname, 'src'),
+        comps: resolve(__dirname, 'src/components'),
+        api: resolve(__dirname, 'src/api'),
+        hooks: resolve(__dirname, 'src/hooks'),
+        types: resolve(__dirname, 'types')
       }
     },
-    rollupOptions: {
-      output: {
-        manualChunks: {
-          axios: ['axios'],
-          vue: ['vue'],
-          'normalize.css': ['normalize.css'],
-          pinia: ['pinia'],
-          'vue-router': ['vue-router']
+    css: {
+      preprocessorOptions: {
+        scss: {
+          additionalData: '@import "src/styles/var/_var.scss";'
+        }
+      }
+    },
+    build: {
+      minify: 'terser',
+      terserOptions: {
+        compress: {
+          // 生产环境时移除console
+          drop_console: VITE_DROP_CONSOLE === 'true',
+          drop_debugger: VITE_DROP_CONSOLE === 'true'
+        }
+      },
+      rollupOptions: {
+        output: {
+          manualChunks: {
+            axios: ['axios'],
+            vue: ['vue'],
+            pinia: ['pinia'],
+            'vue-router': ['vue-router']
+          }
         }
       }
     }
   }
-})
+}
